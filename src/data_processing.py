3b56b8511a1b4fd5b44deb5beb917af119ca5b62
Alexander Kozic <akozic@bowdoin.edu>
2024-04-16 20:45:50 -0400

    Updated player data

--- src/data_processing.py
+++ src/data_processing.py
@@ -6,6 +6,9 @@ df = pd.read_excel('../Stats/Summary.xlsx')
 # Now df is a DataFrame object containing the data from the Excel file
 print(df)  # This prints the first five rows of the DataFrame
 
+# sort data into df's for each team
+
+
 # def create_df():
 #   return 0
 
