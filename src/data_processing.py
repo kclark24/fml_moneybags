b9a28f85077a8ba99a1eeedabfc93ebf68def28d
Kevin Clark <kclark@bowdoin.edu>
2024-05-10 17:55:56 -0400

    Test

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,10 +1,25 @@
 import pandas as pd
+import numpy as np
 
 # Load the Excel file
-df = pd.read_excel('../Stats/Summary.xlsx')
+data = pd.read_excel('../Stats/2022-23 Player Stats.xlsx')
+
+def normalize_data(data):
+    # Define columns not to normalize
+    do_not_normalize = ['Player', 'Season', 'Team', 'S/C', 'Pos', 'GP', 'P/GP', 'S%', 'TOI/GP', 'FOW%']
+
+    # Normalize all other numeric columns by games played
+    for column in data.columns:
+        if column not in do_not_normalize and data[column].dtype in [np.float64, np.int64]:
+            data[column + '/GP'] = data[column] / data['GP']
+    
+    return data
+
+
+
 
 # Now df is a DataFrame object containing the data from the Excel file
-print(df)  # This prints the first five rows of the DataFrame
+print(normalize_data(data))  # This prints the first five rows of the DataFrame
 
 # sort data into df's for each team
 
@@ -14,3 +29,10 @@ print(df)  # This prints the first five rows of the DataFrame
 
 # if __name__ == "__main__":
 #   create_df()
+
+
+
+# train learner on last years stats and test, and then we want to use this seasons data
+
+def normalize_stats(df):
+    pass
