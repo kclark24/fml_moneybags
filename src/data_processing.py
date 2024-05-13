08371df20208cdc5270be1415a6d923900618c15
Kevin Clark <kclark@bowdoin.edu>
2024-05-13 15:02:12 -0400

    Get stats

--- src/data_processing.py
+++ src/data_processing.py
@@ -64,6 +64,29 @@ def get_game_info(game):
     return game_info
 
 
+def average_stats(team_stats):
+
+    stats_list = ['S%', 'FOW%', 'G/GP', 'A/GP', '+/-/GP', 'PIM/GP', 'EVG/GP', 'EVP/GP', 'PPG/GP', 'PPP/GP', 'SHG/GP', 'SHP/GP', 'GWG/GP', 'S/GP']
+
+    average_stats = {stat: 0 for stat in stats_list}
+
+    for stat in stats_list:
+
+        if stat == 'FOW%':
+            holder = 0
+            centers = 0
+            for i in range(len(team_stats[stat])):
+                if team_stats[stat].iloc[i] != '--':
+                    holder += team_stats[stat].iloc[i]
+                    centers += 1
+            average_stats[stat] = holder / centers
+
+        else:
+
+            average_stats[stat] = team_stats[stat].sum() / len(team_stats[stat])
+
+    return average_stats
+
 def calculate_features(game_info):
 
     home_roster_stats = game_info['home_roster_stats']
@@ -72,41 +95,33 @@ def calculate_features(game_info):
     home_goalie_stats = game_info['home_goalie_stats']
     away_goalie_stats = game_info['away_goalie_stats']
 
+    print(home_goalie_stats)
+
     home_win = game_info['home_win']
 
     stats_list = ['S%', 'FOW%', 'G/GP', 'A/GP', '+/-/GP', 'PIM/GP', 'EVG/GP', 'EVP/GP', 'PPG/GP', 'PPP/GP', 'SHG/GP', 'SHP/GP', 'GWG/GP', 'S/GP']
 
-    home_stats = {stat: 0 for stat in stats_list}
-    away_stats = {stat: 0 for stat in stats_list}
+    home_stats = average_stats(home_roster_stats)
+    home_stats['Sv%'] = home_goalie_stats['Sv%']
+    home_stats['GAA'] = home_goalie_stats['GAA']
 
-    for stat in stats_list:
+    away_stats = average_stats(away_roster_stats)
+    away_stats['Sv%'] = away_goalie_stats['Sv%']
+    away_stats['GAA'] = away_goalie_stats['GAA']
 
-        for i in range(len(home_roster_stats)):
-            player = home_roster_stats.iloc[i]
-            print(player)
-            home_stats[stat] += player[stat] / len(home_roster_stats)
 
     # for i in range(len(home_roster_stats)):
     #     player = home_roster_stats.iloc[i]
     #     toi = player['TOI/GP']
-    return home_stats
+    return [home_stats, away_stats, home_win] # here, X is home stats and away stats, y is home_win
 
         
-
-
-    # Want to pass in average per game stats for players and just goalie sv% and gaa (weighed)
-    # we have _/GP * TOI / 60 for what we want to sum up and average
-
-    # Features : S%, TOI/GP, FOW%, G/GP, A/GP, +/-/GP, PIM/GP, EVG/GP, EVP/GP, PPG/GP, PPP/GP, SHG/GP, SHP/GP, GWG/GP, S/GP
-
-
-    # return game_info
         
 
 
 # Example usage with the first item in the game_data DataFrame
 game_data = get_game_info(game_data.iloc[2])
-print(calculate_features(game_data)['home_roster_stats'].iloc[0])
+print(calculate_features(game_data))
 
 # game_roster_stats = get_game_data(game_data.iloc[0]['Home Roster'])
 # print(game_roster_stats)
